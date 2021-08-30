911acb10c18c64eff3e2c859327dae68f0681e62
Damjan9898 <73915350+Damjan9898@users.noreply.github.com>
2021-08-31 00:10:56 +0200

    Add files via upload

---
+++ javascript/script.js
@@ -0,0 +1,3 @@
+document.getElementsByClassName("hamburger")[0].addEventListener("click", function(){
+    document.getElementsByClassName("padajuci-meni")[0].classList.toggle("povecanje-height");
+});
