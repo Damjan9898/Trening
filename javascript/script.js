89df48d9acecfd529c47e67525e44c4074dee357
Damjan9898 <73915350+Damjan9898@users.noreply.github.com>
2021-08-31 00:29:48 +0200

    Delete javascript directory

--- javascript/script.js
+++
@@ -1,3 +0,0 @@
-document.getElementsByClassName("hamburger")[0].addEventListener("click", function(){
-    document.getElementsByClassName("padajuci-meni")[0].classList.toggle("povecanje-height");
-});
